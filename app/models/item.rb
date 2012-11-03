e8be42688516044b9775927854850767be3a12bf
Tamas Leposa <lt@lab85.com>
2012-11-03 16:14:35 +0100

    crop thumbnail instead of resize

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ActiveRecord::Base
 
   has_attached_file :photo,
     :styles => {
-      :thumb => "100x100",
+      :thumb => "100x100#",
       :small  => "150x150",
   	  :big => "300x300"}
 
