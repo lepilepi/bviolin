5901d0089cc497834c3cd8ef47ec6f514f83f985
Tamas Leposa <lt@lab85.com>
2012-11-05 23:23:47 +0100

    the ability to add items to the cart + cart list

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,11 +1,37 @@
 class CartController < ApplicationController
-  def add
+  def update
+    @item = Item.find(params[:id])
+
+    if not session.has_key?(:items)
+      session[:items] = {}
+    end
+
+
+    begin
+      amount=Integer(params[:amount])
+    rescue ArgumentError => e
+      flash[:error] = "sdasdas"
+      return redirect_to action: "show", controller: "items"
+    end
+
+    if session[:items].has_key?(@item)
+        session[:items][@item]+=amount
+    else
+      session[:items][@item]=amount
+    end
+
+    respond_to do |format|
+      format.html { redirect_to action: "index" }
+      format.json { render json: @item, status: :created, location: @item }
+    end
+
   end
 
   def remove
   end
 
-  def list
+  def index
+    @item_hash = session[:items]
   end
 
   def clear
